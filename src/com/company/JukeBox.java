55b89f92fe63d789ccb8d755e3352d6a3fea26bb
VincentBott <vincentbott@gmail.com>
2018-08-09 11:35:50 +0200

    Oef5 op klassen, van tijdens de les op wo 8/8.

---
+++ src/com/company/JukeBox.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class JukeBox {
+
+    private  static final String[] liedjes = {"Bij de rijkswacht", "Teleromeo",
+                                            "Schele Van der Linde"};
+
+
+    public void kiesNummer(int i) {
+
+    }
+
+    public void werpMuntIn() {
+    }
+
+
+    public String speel() {
+
+
+    }
+
+
+    public void verwijderMunten() {
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        JukeBox jukebox = new JukeBox();
+
+        for (int i = 0; i < 10; i++) {
+
+            jukebox.kiesNummer(i);
+
+            jukebox.werpMuntIn();
+
+            System.out.println(jukebox.speel());
+        }
+
+        jukebox.verwijderMunten();
+
+        jukebox.kiesNummer(2);
+
+        jukebox.werpMuntIn();
+
+        System.out.println(jukebox.speel());
     }
 }
